91b5c062f29b885e55870894df54352f7745b4f2
yellow-redcard <iclever0507@gmail.com>
2024-10-15 21:18:24 +0900

    [Update]ProjectileController

---
+++ Dodge.C_Learn/Assets/Scripts/Controllers/ProjectileController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileController : MonoBehaviour
+{
+    private Rigidbody2D rigidbody2D;
+    float speed = 0.5f;
+
+
+    private void Awake()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+    
+    public void Shoot(Vector3 vec)
+    {
+        rigidbody2D.velocity = vec*speed;
+    }
+    public void RandomShoot()
+    {
+        Vector2 direction = Random.insideUnitCircle.normalized;
+        rigidbody2D.velocity = direction * 0.2f;
+    }
+}
