37f776bef25c6bcf132122b045bf6fada455c59d
moloch-kim <79761359+moloch-kim@users.noreply.github.com>
2024-10-15 18:00:05 +0900

    Revert "Add Sprite and Animation"
    This reverts commit 8188e6e95ab99400a53ad9a18363b672ea0b7905.

--- Dodge.C_Learn/Assets/Scripts/Controllers/PlayerController.cs
+++ Dodge.C_Learn/Assets/Scripts/Controllers/PlayerController.cs
@@ -8,17 +8,11 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float speed = 5.0f;
     private Rigidbody2D rb;
     private Vector2 moveInput;
-    /// <summary>
-    /// rigidbody2D 가져오기
-    /// </summary>
+
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
     }
-    /// <summary>
-    /// WASD 입력시 발동
-    /// </summary>
-    /// <param name="value"></param>
     void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>().normalized;
