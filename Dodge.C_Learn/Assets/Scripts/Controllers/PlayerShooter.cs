f167cb2e5c06c6546d3876cc4a06a9ca55093682
moloch-kim <79761359+moloch-kim@users.noreply.github.com>
2024-10-16 20:11:23 +0900

    Apply PoolManager

--- Dodge.C_Learn/Assets/Scripts/Controllers/PlayerShooter.cs
+++ Dodge.C_Learn/Assets/Scripts/Controllers/PlayerShooter.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.Antlr3.Runtime.Tree;
 using UnityEngine;
 
 public class PlayerShooter : MonoBehaviour
@@ -10,7 +11,6 @@ public class PlayerShooter : MonoBehaviour
     public float projectileSpeed = 100f;
     float time = 0f;
     public float fireRate;
-
     public float Power;
 
     void Shoot()
@@ -18,51 +18,39 @@ public class PlayerShooter : MonoBehaviour
         switch (Power)
         {
             case 0:
-                GameObject bullet = Instantiate(projectileA, firePoint.position, firePoint.rotation);
-                Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
-                rb.velocity = firePoint.up * projectileSpeed;
+                SpawnBullet(ObjectType.ProjectileA, Vector3.zero);
                 break;
             case 1:
-                GameObject bullet1 = Instantiate(projectileA, firePoint.position + Vector3.right * 0.08f, firePoint.rotation);
-                GameObject bullet2 = Instantiate(projectileA, firePoint.position + Vector3.left * 0.08f, firePoint.rotation);
-                Rigidbody2D rb1 = bullet1.GetComponent<Rigidbody2D>();
-                Rigidbody2D rb2 = bullet2.GetComponent<Rigidbody2D>();
-                rb1.velocity = firePoint.up * projectileSpeed;
-                rb2.velocity = firePoint.up * projectileSpeed;
+                SpawnBullet(ObjectType.ProjectileA, Vector3.right * 0.08f);
+                SpawnBullet(ObjectType.ProjectileA, Vector3.left * 0.08f);
                 break;
             case 2:
-                GameObject bulleta = Instantiate(projectileA, firePoint.position + Vector3.right * 0.1f, firePoint.rotation);
-                GameObject bulletb = Instantiate(projectileA, firePoint.position + Vector3.left * 0.1f, firePoint.rotation);
-                GameObject bulletc = Instantiate(projectileA, firePoint.position + Vector3.up * 0.1f, firePoint.rotation);
-                Rigidbody2D rba = bulleta.GetComponent<Rigidbody2D>();
-                Rigidbody2D rbb = bulletb.GetComponent<Rigidbody2D>();
-                Rigidbody2D rbc = bulletc.GetComponent<Rigidbody2D>();
-                rba.velocity = firePoint.up * projectileSpeed;
-                rbb.velocity = firePoint.up * projectileSpeed;
-                rbc.velocity = firePoint.up * projectileSpeed;
+                SpawnBullet(ObjectType.ProjectileA, Vector3.right * 0.1f);
+                SpawnBullet(ObjectType.ProjectileA, Vector3.left * 0.1f);
+                SpawnBullet(ObjectType.ProjectileA, Vector3.up * 0.1f);
                 break;
             case 3:
-                GameObject bulletaa = Instantiate(projectileA, firePoint.position + Vector3.right * 0.1f, firePoint.rotation);
-                GameObject bulletbb = Instantiate(projectileA, firePoint.position + Vector3.left * 0.1f, firePoint.rotation);
-                GameObject bulletcc = Instantiate(projectileA, firePoint.position + Vector3.right * 0.2f, firePoint.rotation);
-                GameObject bulletdd = Instantiate(projectileA, firePoint.position + Vector3.left * 0.2f, firePoint.rotation);
-                Rigidbody2D rbaa = bulletaa.GetComponent<Rigidbody2D>();
-                Rigidbody2D rbbb = bulletbb.GetComponent<Rigidbody2D>();
-                Rigidbody2D rbcc = bulletcc.GetComponent<Rigidbody2D>();
-                Rigidbody2D rbdd = bulletdd.GetComponent<Rigidbody2D>();
-                rbaa.velocity = firePoint.up * projectileSpeed;
-                rbbb.velocity = firePoint.up * projectileSpeed;
-                rbcc.velocity = firePoint.up * projectileSpeed;
-                rbdd.velocity = firePoint.up * projectileSpeed;
+                SpawnBullet(ObjectType.ProjectileA, Vector3.right * 0.1f);
+                SpawnBullet(ObjectType.ProjectileA, Vector3.right * 0.2f);
+                SpawnBullet(ObjectType.ProjectileA, Vector3.left * 0.1f);
+                SpawnBullet(ObjectType.ProjectileA, Vector3.left * 0.2f);
                 break;
             case 4:
-                GameObject Bullet = Instantiate(projectileB, firePoint.position , firePoint.rotation);
-                Rigidbody2D RB = Bullet.GetComponent<Rigidbody2D>();
-                RB.velocity = firePoint.up * projectileSpeed;
+                SpawnBullet(ObjectType.ProjectileB, Vector3.zero);
                 break;
-
+            case 5:
+                SpawnBullet(ObjectType.ProjectileB, Vector3.up * 0.1f);
+                SpawnBullet(ObjectType.ProjectileA, Vector3.right * 0.15f);
+                SpawnBullet(ObjectType.ProjectileA, Vector3.left * 0.15f);
+                break;
         }
-        
+    }
+
+    private void SpawnBullet(ObjectType type , Vector3 vec)
+    {
+        GameObject bullet = ObjectPoolManager.Instance.GetObject(type, firePoint , vec);
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.velocity = firePoint.up * projectileSpeed;
     }
 
     private void Update()
