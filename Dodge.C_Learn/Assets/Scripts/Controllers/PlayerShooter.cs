71b414fb654930baea3b6d85137138b8ac9eb4a9
moloch-kim <79761359+moloch-kim@users.noreply.github.com>
2024-10-16 15:14:52 +0900

    Adjust Projectile logic

--- Dodge.C_Learn/Assets/Scripts/Controllers/PlayerShooter.cs
+++ Dodge.C_Learn/Assets/Scripts/Controllers/PlayerShooter.cs
@@ -4,16 +4,40 @@ using UnityEngine;
 
 public class PlayerShooter : MonoBehaviour
 {
-    public GameObject projectile;
+    public GameObject projectileA;
+    public GameObject projectileB;
     public Transform firePoint;
     public float projectileSpeed = 100f;
     float time = 0f;
     public float fireRate;
+
+    public float Power;
+
     void Shoot()
     {
-        projectile = Instantiate(projectile, firePoint.position, firePoint.rotation);
-        Rigidbody2D rb = projectile.GetComponent<Rigidbody2D>();
-        rb.velocity = firePoint.up * projectileSpeed;
+        switch (Power)
+        {
+            case 0:
+                GameObject bullet = Instantiate(projectileA, firePoint.position, firePoint.rotation);
+                Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+                rb.velocity = firePoint.up * projectileSpeed;
+                break;
+            case 1:
+                GameObject bullet1 = Instantiate(projectileA, firePoint.position + Vector3.right * 0.1f, firePoint.rotation);
+                GameObject bullet2 = Instantiate(projectileA, firePoint.position + Vector3.left * 0.1f, firePoint.rotation);
+                Rigidbody2D rb1 = bullet1.GetComponent<Rigidbody2D>();
+                Rigidbody2D rb2 = bullet2.GetComponent<Rigidbody2D>();
+                rb1.velocity = firePoint.up * projectileSpeed;
+                rb2.velocity = firePoint.up * projectileSpeed;
+                break;
+            case 2:
+                //GameObject bullet1 = Instantiate(projectileA, firePoint.position + Vector3.right * 0.1f, firePoint.rotation);
+                //GameObject bullet2 = Instantiate(projectileA, firePoint.position + Vector3.right * 0.1f, firePoint.rotation);
+                //GameObject bullet3 = Instantiate(projectileA, firePoint.position + Vector3.right * 0.1f, firePoint.rotation);
+                break;
+
+        }
+        
     }
 
     private void Update()
