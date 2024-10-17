ef7542f625c14732e3be569eb61ab606dc367f05
moloch-kim <79761359+moloch-kim@users.noreply.github.com>
2024-10-17 20:33:37 +0900

    Apply Patterned Attack

--- Dodge.C_Learn/Assets/Scripts/Controllers/EnemyShooter.cs
+++ Dodge.C_Learn/Assets/Scripts/Controllers/EnemyShooter.cs
@@ -12,31 +12,47 @@ public class EnemyShooter : MonoBehaviour
 
     bool isCooldown = false;
 
-    int curPatternCount = 0;
-    int maxPatternCount;
+    public EnemyType ET;
+
+    int num = 0;
+
     private void Update()
     {
-        time += Time.deltaTime;
         if (!isCooldown)
         {
-            if (time >= firerate)
-            {
-                ArcFire();
-                time = 0f;
-            }
+            ApplyAttack();
+            Reloading();
         }
         else
         {
-            isCooldown = false;
+            time += Time.deltaTime;
+            if (time >= firerate)
+            {
+                isCooldown = false;
+                time = 0f;
+            }
         }
     }
-    private void Fire()
+
+    private void ApplyAttack()
     {
-        time = Time.deltaTime;
-        if (time >= firerate)
+        switch( ET )
         {
-            Shoot();
-            time = 0f;
+            case EnemyType.Corvette:
+                Fire();
+                break;
+            case EnemyType.Frigate:
+                FireBurst(2);
+                break;
+            case EnemyType.Destroyer:
+                FireAround(12, 1);
+                break;
+            case EnemyType.Cruiser:
+                FireArc(50);
+                break;
+            case EnemyType.Battleship:
+                FireAround(50, 5);
+                break;
         }
     }
 
@@ -50,29 +66,54 @@ public class EnemyShooter : MonoBehaviour
         Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
         rb.velocity = Vector2.down * projectileSpeed;
     }
-    private void BurstFire()
+    private void Fire() // 일반적인 사격
     {
-        for(int i = 0; i < 4; i++)
+        Shoot();
+        Reloading();
+    }
+    private void FireBurst(int fireamount) // 점사
+    {
+        for(int i = 0; i < fireamount; i++)
         {
             Invoke("Shoot", i * 0.1f);
         }
+        Reloading();
     }
-
-    private void ArcFire()
+    private void FireArc(int fireamount) // 호를 그리면서 사격
     {
-        maxPatternCount = 50;
-        projectileSpeed = 3f;
+        projectileSpeed = 2f;
         GameObject bullet = ObjectPoolManager.Instance.GetObject(ObjectType.EnemyProjectile);
         Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
         bullet.transform.position = transform.position;
         bullet.transform.rotation = Quaternion.identity;
 
-        Vector2 dirVec = new Vector2(Mathf.Sin(Mathf.PI* 2 * curPatternCount/maxPatternCount), -1);
+        Vector2 dirVec = new Vector2(Mathf.Sin(Mathf.PI* 2 * num /fireamount), -1);
         rb.velocity = dirVec * projectileSpeed;
 
-        curPatternCount++;
-        if (curPatternCount < maxPatternCount)
+        num++;
+        if (num < fireamount)
             Invoke("ArcFire", 0.1f);
+        else
+        {
+            projectileSpeed = 5f;
+        }
+    }
+    private void FireAround(int roundamount ,int firecount) // 원 형태로 사격
+    {
+        projectileSpeed = 2f;
+        for (int i = 0; i < roundamount; i++)
+        {
+            GameObject bullet = ObjectPoolManager.Instance.GetObject(ObjectType.EnemyProjectile);
+            Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+            bullet.transform.position = transform.position;
+            bullet.transform.rotation = Quaternion.identity;
+
+            Vector2 dirVec = new Vector2(Mathf.Cos(Mathf.PI * 2 * i / roundamount), Mathf.Sin(Mathf.PI * 2 * i / roundamount));
+            rb.velocity = dirVec * projectileSpeed;
+        }
+        num++;
+        if (num < firecount)
+            Invoke("FireAround", 1f);
         else
         {
             Reloading();
@@ -82,6 +123,6 @@ public class EnemyShooter : MonoBehaviour
     private void Reloading()
     {
         isCooldown = true;
-        curPatternCount = 0;
+        num = 0;
     }
 }
