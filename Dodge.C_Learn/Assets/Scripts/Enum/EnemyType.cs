5fe7d543f9413d5612a02c14cc1d7bcfb3507cf1
moloch-kim <79761359+moloch-kim@users.noreply.github.com>
2024-10-17 13:43:07 +0900

    Update Enemy

--- Dodge.C_Learn/Assets/Scripts/Enum/EnemyType.cs
+++ Dodge.C_Learn/Assets/Scripts/Enum/EnemyType.cs
@@ -1,5 +1,8 @@
 public enum EnemyType
 {
-    Easy = 0,
-    Normal,
-}
+    Corvette , // 초계함 체력 1
+    Frigate , // 호위함  체력 5
+    Destroyer, // 구축함 체력 15
+    Cruiser, // 순양함 체력 25
+    Battleship, // 전함 체력 50
+}   
--- Dodge.C_Learn/Assets/Scripts/Enum/ObjectType.cs
+++ Dodge.C_Learn/Assets/Scripts/Enum/ObjectType.cs
@@ -5,6 +5,12 @@ public enum ObjectType
 {
     Object = 0,
     Effect,
-    ProjectileA,
-    ProjectileB
+    ProjectileA, // 1데미지
+    ProjectileB, // 5데미지
+    EnemyProjectile, // 적 투사체
+    Corvette, // 초계함
+    Frigate, // 호위함
+    Destroyer, // 구축함
+    Cruiser, // 순양함
+    Battleship, // 전함
 }
