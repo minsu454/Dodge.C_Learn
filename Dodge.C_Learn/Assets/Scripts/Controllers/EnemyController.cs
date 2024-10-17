c9887deb181d39763603987c81c9105648ec2ffb
moloch-kim <79761359+moloch-kim@users.noreply.github.com>
2024-10-17 09:59:00 +0900

    Add Enemy

---
+++ Dodge.C_Learn/Assets/Scripts/Controllers/EnemyController.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float health;
+    public float maxHealth;
+    public float speed;
+    public GameObject projectile;
+    
+}
